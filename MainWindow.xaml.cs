c070ebb25b164b5395cfd17f4d10411ac279e762
lr1112 <luis_rosario_0324@hotmail.com>
2021-11-18 14:44:03 -0400

    Creando las entidades, instalando Nugets, creando migracion y update-database

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto_Final_Repuesto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rUsuariosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rClientesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rTiposProductosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rProductosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rVentasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rCobrosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cUsuariosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cClientesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cTiposProductosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cProductosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cVentasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cCobrosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
