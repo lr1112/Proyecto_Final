fe3f4af9451e3ab79fe2561be478e8caae238f86
lr1112 <luis_rosario_0324@hotmail.com>
2021-11-22 17:23:47 -0400

    Agregando la consulta de TiposProducto  y Cobros

---
+++ UI/Consultas/cCobros.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Final_Repuesto.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cCobros.xaml
+    /// </summary>
+    public partial class cCobros : Window
+    {
+        public cCobros()
+        {
+            InitializeComponent();
+        }
+
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
