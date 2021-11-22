02e5b6c4dc35474c715b4898294644fd8124a42d
lr1112 <luis_rosario_0324@hotmail.com>
2021-11-22 16:50:57 -0400

    Agregando al proyecto la consulta de ventas y de productos

--- UI/Consultas/cUsuarios.xaml.cs
+++ UI/Consultas/cUsuarios.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,11 +24,61 @@ namespace Proyecto_Final_Repuesto.UI.Consultas
         public cUsuarios()
         {
             InitializeComponent();
+            DesdeDataPicker.SelectedDate = Convert.ToDateTime("1/01/0001");
+            HastaDataPicker.SelectedDate = DateTime.Now;
+        }
+
+        private List<dynamic> GetDisplay(List<Usuarios> lista)
+        {
+            var listado = new List<dynamic>();
+
+            foreach (var usuario in lista)
+            {
+                var user = new
+                {
+                    usuario.UsuarioId,
+                    usuario.Nombres,
+                    usuario.Apellidos,
+                    Fecha = usuario.Fecha.ToString("dd/MM/yyy"),
+                    usuario.NombreUsuario,
+                    Permisos = usuario.EsAdmin == 1 ? "Administrador" : "Empleado",
+                    UsuarioModificador = usuario.UsuarioModificador != 0 ? UsuariosBLL.Buscar(usuario.UsuarioModificador).NombreUsuario : "Default"
+                };
+
+                listado.Add(user);
+            }
+
+            return listado;
         }
 
         private void ConsultarButton_Click(object sender, RoutedEventArgs e)
         {
+            var listado = new List<Usuarios>();
+            List<dynamic> list = new List<dynamic>();
+            if (string.IsNullOrWhiteSpace(CriterioTextBox.Text))
+            {
+                listado = UsuariosBLL.GetList(e => true);
+                list = GetDisplay(listado);
+            }
+            else
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = UsuariosBLL.GetList(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate && c.Fecha.Date <= HastaDataPicker.SelectedDate).FindAll(e => e.UsuarioId == Convert.ToInt32(CriterioTextBox.Text));
+                        list = GetDisplay(listado);
+                        break;
+                    case 1:
+                        listado = UsuariosBLL.GetList(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate && c.Fecha.Date <= HastaDataPicker.SelectedDate).FindAll(e => e.NombreUsuario.Contains(CriterioTextBox.Text));
+                        list = GetDisplay(listado);
+                        break;
+                }
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = list;
 
         }
+            
     }
 }
